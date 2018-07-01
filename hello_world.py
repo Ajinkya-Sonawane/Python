62bf5fa55a64b73bc677c6ac776d3a017f6fb0d3
Ajinkya-Sonawane <sonawaneajinks@gmail.com>
2018-07-01 12:10:35 +0530

    Updated Hello World

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 #First Program : Hello World , by Python
 #- Ajinkya Sonawane [AJ-CODE-7]
 
-print("Hello World , by Python")
+print("Hello World , by Python - Ajinkya ")
 
 ''' print() is an inbuilt function that takes strings/values or variables as
     input and displays on the screen '''
